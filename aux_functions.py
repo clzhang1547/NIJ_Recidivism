9825c056610d03bd9b81419c20f81a013f2b2c8a
clzhang1547 <lzhang1547@gmail.com>
2021-05-20 23:26:49 -0700

    DA with random 0 first implementation

--- aux_functions.py
+++ aux_functions.py
@@ -6,6 +6,7 @@
 import pandas as pd
 import random
 from sklearn.preprocessing import OneHotEncoder
+import numpy as np
 
 # a function to read and clean up raw data
 def read_and_clean_raw(fp):
@@ -52,3 +53,29 @@ def get_bool_binary_cols(df):
                  if len(df[c].dropna().value_counts().index)==2]
     two_class_cols = list(set(two_class_cols) - set(bool_cols) - set(binary_cols))
     return (bool_cols, binary_cols, two_class_cols)
+
+# a function to randomly set 0s to 2d array
+def set_random_elements(array, prop, fill=0):
+    # array = 2d array with elements in list (so actually 3d), prop = proprtion between 0 to 1, fill = value to fill in
+    fill_size = int(array.size*prop) # number of fills to perform
+    random_idxs0 = np.random.randint(0, array.shape[0], size=fill_size)
+    random_idxs1 = np.random.randint(0, array.shape[1], size=fill_size)
+    filled_array = array.copy()
+    filled_array[random_idxs0, random_idxs1] = fill
+    return filled_array
+
+def set_random_nonzero_elements(array, prop, fill=0):
+    # array = 2d array with elements in list (so actually 3d), prop = proprtion between 0 to 1, fill = value to fill in
+    idxs_nonzero = np.nonzero(array.reshape(-1, array.shape[1]))
+    idxs_nonzero = list(zip(idxs_nonzero[0], idxs_nonzero[1]))
+    fill_size = int(len(idxs_nonzero)*prop) # number of fills to perform
+    random_idxs_spots = np.random.choice(len(idxs_nonzero), size=fill_size) # location of chosen index tuples in nonzero list
+
+    random_idxs = np.array(idxs_nonzero)[random_idxs_spots] # chosen indices to be filled
+    # random indices along both axes
+    random_idxs0 = random_idxs[:, 0]
+    random_idxs1 = random_idxs[:, 1]
+
+    filled_array = array.copy()
+    filled_array[random_idxs0, random_idxs1] = fill
+    return filled_array
--- da_nij.py
+++ da_nij.py
@@ -6,13 +6,16 @@
 # TODO [done]: check DA loss graph on denoising x_train_noisy <- noise factor=0.01 works for NIJ data
 # ^noise factor=0.02 divegence occurs, 0.1, 0.5 bad
 # TODO [done]: add constant xvars so #cols = 4X, 8X allowing encoding/decoding flexibility to lower dim
+# TODO: noise=0.5 good, pred on d (no noise). Try noise=1, pred on d, until noise too large (hurting perf)
+#^cannot overwrite autoencoder obj with new noise_factor ow diverging, rerun code to ensure valid result
 # TODO: set noise a la Beaulieu-Jones 2016: randomly set to 0 for 20% values of input matrix
-# TODO: Year 1 results with 128>>64 xvars and 0.02 noise get similar/worse perf vs ML. Try 32 xvars, random 0 as noise
+# TODO: Year 1 results with 128>>64 xvars and 0.5 noise get similar/worse perf vs ML. Try 32 or 8 xvars, random 0 as noise
+# TODO: add in ACS/GA crime xvars (re Mason/Sandeep)
 ##################################
 
 import os
 os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="1" #model will be trained on GPU 1
+os.environ["CUDA_VISIBLE_DEVICES"]="0" #model will be trained on GPU 0
 
 import keras
 from matplotlib import pyplot as plt
@@ -118,6 +121,7 @@ def autoencoder(input_img):
     pool2 = MaxPooling1D(pool_size=2)(conv2) # k/2 x 1 x 64
     conv3 = Conv1D(128, (3), activation='relu', padding='same')(pool2) # k/4 x 1 x 128 (small and thick)
 
+
     #decoder
     conv4 = Conv1D(128, (3), activation='relu', padding='same')(conv3) # k/4 x 1 x 128
     up1 = UpSampling1D(2)(conv4) # k/2 x 1 x 128
@@ -127,6 +131,7 @@ def autoencoder(input_img):
     return decoded
 
 # def autoencoder(input_img):
+#     # a deeper CNN with lowest phenotype dim to k/16
 #     # Note - k should be dividable by pool_size multiple times, as set by conv structure
 #     #encoder
 #     #input = k x 1 (wide and thin)
@@ -144,8 +149,12 @@ def autoencoder(input_img):
 #     conv6 = Conv1D(512, (3), activation='relu', padding='same')(conv5) # k/16 x 1 x 512
 #     up1 = UpSampling1D(2)(conv6) # k/8 x 1 x 512
 #     conv7 = Conv1D(256, (3), activation='relu', padding='same')(up1) # k/8 x 1 x 256
-#     up2 = UpSampling1D(2)(conv7) # k/4 x 1 x 64
-#     decoded = Conv1D(1, (3,), activation='sigmoid', padding='same')(up2) # k/4 x 1 x 1
+#     up2 = UpSampling1D(2)(conv7) # k/4 x 1 x 256
+#     conv8 = Conv1D(128, (3,), activation='relu', padding='same')(up2) # k/4 x 1 x 128
+#     up3 = UpSampling1D(2)(conv8) # k/2 x 1 x 129
+#     conv9 = Conv1D(64, (3,), activation='relu', padding='same')(up3) # k/2 x 1 x 64
+#     up4 = UpSampling1D(2)(conv9) # k x 1 x 64
+#     decoded = Conv1D(1, (3,), activation='sigmoid', padding='same')(up4) # k x 1 x 1
 #     return decoded
 
 autoencoder = Model(input_img, autoencoder(input_img))
@@ -172,15 +181,18 @@ pred = autoencoder.predict(train_X)
 print(pred - train_ground) # should see small errors
 
 # Add noise to train/valid data
-noise_factor = 0.02 # 0.5 on NIJ data would cause valid loss diverging up, 0.01 is good for convergence
-x_train_noisy = train_X + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=train_X.shape)
-x_valid_noisy = valid_X + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=valid_X.shape)
-x_train_noisy = np.clip(x_train_noisy, 0., 1.)
-x_valid_noisy = np.clip(x_valid_noisy, 0., 1.)
+# noise_factor = 0.5 # 0.5 on NIJ data would cause valid loss diverging up, 0.01 is good for convergence
+# x_train_noisy = train_X + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=train_X.shape)
+# x_valid_noisy = valid_X + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=valid_X.shape)
+# x_train_noisy = np.clip(x_train_noisy, 0., 1.)
+# x_valid_noisy = np.clip(x_valid_noisy, 0., 1.)
+noise_prop = 0.2
+x_train_noisy = set_random_nonzero_elements(train_X, noise_prop, fill=0)
+x_valid_noisy = set_random_nonzero_elements(valid_X, noise_prop, fill=0)
 
 # Denoise Autoencoder Hyperparameters
 batch_size = 128
-epochs = 50
+epochs = 20
 input_img = Input(shape = (k, 1))
 
 autoencoder = Model(input_img, autoencoder(input_img))
@@ -190,34 +202,60 @@ autoencoder_train = autoencoder.fit(x_train_noisy, train_X, batch_size=batch_siz
                                     verbose=1,validation_data=(x_valid_noisy, valid_X))
 
 # Get intermediate layer (phenotype) from trained model
-layer_name = 'up_sampling1d_8'
+layer_name = 'up_sampling1d' # second last UpSampling1D layer
 intermediate_layer_model = Model(inputs=autoencoder.input,
                                  outputs=autoencoder.get_layer(layer_name).output)
-d_noisy = d + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=d.shape)
-intermediate_output = intermediate_layer_model.predict(d_noisy) # N x k/4 x 128, get phenotype for entire dataset d
+intermediate_output = intermediate_layer_model.predict(d) # N x k/4 x 128, get phenotype for entire dataset d
 phenotype = Conv1D(1, (3,), activation='sigmoid', padding='same')(intermediate_output)  # N x k/4 x 1
-
+phenotype = phenotype.numpy()[:,:,0]
 # XGBoost for phenotype and train_labels
 clf = xgboost.XGBClassifier(objective='binary:logistic', use_label_encoder=False) # one hot encoding
-clf = LogisticRegression(max_iter=1000)
-clf = RandomForestClassifier()
+# clf = LogisticRegression(max_iter=1000)
+# clf = RandomForestClassifier()
 scores = ['roc_auc', 'f1', 'precision', 'recall', 'accuracy', 'neg_brier_score']
 # Model - Year 1
 y = train_labels
-X = phenotype.numpy()[0]
+X = phenotype
 dct_score = {}
 for s in scores:
-    dct_score[s] = round(cross_val_score(clf, X, y, cv=5, scoring=s).mean(), 4)
-    print('CV score completed -- %s' % s)
+    v = round(cross_val_score(clf, X, y, cv=5, scoring=s).mean(), 4)
+    dct_score[s] = v
+    print('CV score completed -- %s=%s' % (s, v))
 print(dct_score)
 
 '''
-DA - XGBoost
-Year 1: {'roc_auc': 0.6054, 'f1': 0.2738, 'precision': 0.4233, 'recall': 0.2036, 'accuracy': 0.679, 'neg_brier_score': -0.2174}
+DA - XGBoost (noise_factor = 0.5)
+Year 1: {'roc_auc': 0.632, 'f1': 0.2987, 'precision': 0.4462, 'recall': 0.225, 'accuracy': 0.6853, 'neg_brier_score': -0.2114}
 
-DA - Logit
-Year 1: {'roc_auc': 0.6362, 'f1': 0.0059, 'precision': 0.6143, 'recall': 0.003, 'accuracy': 0.7022, 'neg_brier_score': -0.2013}
+DA - XGBoost (noise_factor = 0.5, NN depth = 5, n_type = 16, DA CV5 slightly diverge)
+Year 1: {'roc_auc': 0.5797, 'f1': 0.209, 'precision': 0.3897, 'recall': 0.143, 'accuracy': 0.6774, 'neg_brier_score': -0.2171}
+
+DA - Logit (noise_factor = 0.02)
+Year 1: {'roc_auc': 0.6411, 'f1': 0.0232, 'precision': 0.5125, 'recall': 0.0119, 'accuracy': 0.702, 'neg_brier_score': -0.1997}
+
+DA - Random Forest (noise_factor = 0.02)
+Year 1: {'roc_auc': 0.6389, 'f1': 0.1057, 'precision': 0.4685, 'recall': 0.0612, 'accuracy': 0.7011, 'neg_brier_score': -0.1992}
+
+======================= DA with random 20% 0s on nonzero elements =====================
+DA0 - XGBoost
+Year 1: {'roc_auc': 0.6383, 'f1': 0.3048, 'precision': 0.4465, 'recall': 0.2325, 'accuracy': 0.685, 'neg_brier_score': -0.2102}
+
+DA0 - XGBoost (train epoch=20)
+Year 1: {'roc_auc': 0.6363, 'f1': 0.2941, 'precision': 0.4374, 'recall': 0.2222, 'accuracy': 0.6819, 'neg_brier_score': -0.2115}
+
+DA0 - Logit
+Year 1: {'roc_auc': 0.6373, 'f1': 0.0388, 'precision': 0.4268, 'recall': 0.0206, 'accuracy': 0.7015, 'neg_brier_score': -0.2002}
+
 '''
+# #
+# x = np.array([[[1],[2],[3]], [[4],[5],[6]], [[7],[8],[9]]])
+# y = set_random_elements(x, 0.5,999)
+#
+# z = set_random_elements(train_X, 0.2, 999)
+#
+# random_idxs0 = np.random.randint(0, x.shape[0], size=2)
+# random_idxs1 = np.random.randint(0, x.shape[1], size=2)
+# x[random_idxs0, random_idxs1] = 0 # inplace
 
 
 
